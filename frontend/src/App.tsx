3562be5453190e5f2173a1096be91ad24f6e7a12
Felipe <fraifelipe@gmail.com>
2020-03-27 20:39:11 -0300

    drag and drop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,63 +1,26 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import "./App.css";
-import {
-  Card,
-  CardContent,
-  Typography,
-  CardActions,
-  makeStyles
-} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import MainCard from "./components/MainCard";
 
 function App() {
   const classes = useStyles();
-  return (
-    <div className="App">
-      <Card className={classes.root}>
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="h2">
-            a
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            adjective
-          </Typography>
-          <Typography variant="body2" component="p">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
-        </CardActions>
-      </Card>
-    </div>
-  );
+
+  setTimeout(() => alert("EPA!"), 5000);
+
+  return <div className={classes.App}>{<MainCard />}</div>;
 }
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 775
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
+  App: {
+    textAlign: "center",
+    backgroundColor: "#282c34",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white"
   }
 });
 
--- frontend/src/components/MainCard.tsx
+++ frontend/src/components/MainCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import "./App.css";
+import Example from "./Example";
 import {
   Card,
   CardContent,
@@ -8,37 +8,41 @@ import {
   CardActions,
   makeStyles
 } from "@material-ui/core";
+import { DndProvider } from "react-dnd";
+import Backend from "react-dnd-html5-backend";
 
 function MainCard() {
   const classes = useStyles();
   return (
-      <Card className={classes.root}>
-        <CardContent>
-          <Typography
-            className={classes.title}
-            color="textSecondary"
-            gutterBottom
-          >
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="h2">
-            a
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            adjective
-          </Typography>
-          <Typography variant="body2" component="p">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
-        </CardActions>
-      </Card>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Mobster
+        </Typography>
+        <Typography variant="h5" component="h2">
+          a
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+
+        <DndProvider backend={Backend}>{<Example />}</DndProvider>
+      </CardContent>
+      <CardActions>
+        <Button className={classes.button} variant="contained" color="primary">
+          Start Mob
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
 
@@ -52,11 +56,16 @@ const useStyles = makeStyles({
     transform: "scale(0.8)"
   },
   title: {
-    fontSize: 14
+    fontSize: 35,
+    color: "black"
   },
   pos: {
     marginBottom: 12
+  },
+  button: {
+    alignSelf: "center",
+    margin: "0 auto"
   }
 });
 
-export default App;
+export default MainCard;
