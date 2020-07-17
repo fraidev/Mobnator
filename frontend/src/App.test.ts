7bfc6e0177412a58ba41d63fcc1e432e0aa673a1
Felipe Cardozo <fraifelipe@gmail.com>
2020-07-17 20:15:54 -0300

    tests

--- frontend/src/App.test.ts
+++ frontend/src/App.test.ts
@@ -1,6 +1,4 @@
+import { render } from '@testing-library/react'
 
 test('renders learn react link', () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
 })
