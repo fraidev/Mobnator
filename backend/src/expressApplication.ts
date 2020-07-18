150872b017bd8ab06b69341f3655c11db01b0fc4
Felipe Cardozo <fraifelipe@gmail.com>
2020-07-17 22:06:56 -0300

    fix cors again

--- backend/src/expressApplication.ts
+++ backend/src/expressApplication.ts
@@ -9,6 +9,12 @@ const server = () => {
   const middlewares = () => {
     expressApplication.use(bodyParser.json())
     expressApplication.use(bodyParser.urlencoded({ extended: true }))
+
+    expressApplication.use(function (req, res, next) {
+      res.header('Access-Control-Allow-Origin', 'https://mobnator.com https://www.mobnator.com https://be.mobnator.com')
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+      next()
+    })
     const corsOptions = {
       origin: '*',
       optionsSuccessStatus: 200
